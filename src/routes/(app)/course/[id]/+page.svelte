7bf727ec40252f5a9b534d9025844234d7363267
GiorgiUbiria <ubiriagiorgi8@gmail.com>
2024-03-07 19:49:50 +0400

    Added id prop to the Card component. Added slug retrieval for course individual page.

--- src/routes/(app)/course/[id]/+page.svelte
+++ src/routes/(app)/course/[id]/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
+	import { page } from '$app/stores';
 
+	const id: string = $page.params.id;
 </script>
 
-<h1> africa</h1>
+<h1>{id}</h1>
--- src/routes/(app)/courses/+page.svelte
+++ src/routes/(app)/courses/+page.svelte
@@ -2,11 +2,9 @@
 	import Card from '../components/card.svelte';
 </script>
 
-<div
-	class="w-full mx-auto py-16 grid grid-cols-2 gap-12 place-items-center text-white"
->
-	<Card />
-	<Card />
-	<Card />
-	<Card />
+<div class="w-full mx-auto py-16 grid grid-cols-2 gap-12 place-items-center text-white">
+	<Card id={'1'} />
+	<Card id={'1'} />
+	<Card id={'1'} />
+	<Card id={'1'} />
 </div>
