982b187bce45c8f83fcb29db7e932ebe1218252e
GiorgiUbiria <ubiriagiorgi8@gmail.com>
2024-03-12 21:56:43 +0400

    Fixed navigation bar. Added navigation from content page to dashboard foradmin.

--- src/routes/(regular)/components/navbar.svelte
+++ src/routes/(regular)/components/navbar.svelte
@@ -3,43 +3,103 @@
 	import type { User } from '@supabase/supabase-js';
 
 	export let user: User | undefined;
+
+	let isOpen = false;
+
+	function toggleMenu() {
+		isOpen = !isOpen;
+	}
 </script>
 
-<nav
-	class="flex flex-col md:container mx-auto gap-4 md:gap-0 px-6 py-8 md:py-4 md:flex md:flex-row justify-between items-center"
->
-	<a
-		href="/"
-		aria-current={$page.url.pathname === '/'}
-		class="text-white text-2xl md:text-4xl font-bold">PR WAVE</a
-	>
-	<div class="flex space-x-4">
-		{#if user !== undefined}
-			<a
-				href="/admin/dashboard"
-				aria-current={$page.url.pathname === '/admin/dashboard'}
-				class="text-white hover:text-gray-300">DASHBOARD</a
+<nav class="border-gray-200 dark:bg-gray-900">
+	<div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+		<a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
+			<img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
+			<span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"
+				>PR WAVE</span
 			>
-		{/if}
-		<a
-			href="/courses"
-			aria-current={$page.url.pathname === '/courses'}
-			class="text-white hover:text-gray-300">COURSES</a
+		</a>
+		<button
+			data-collapse-toggle="navbar-default"
+			on:click={toggleMenu}
+			type="button"
+			class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+			aria-controls="navbar-default"
+			aria-expanded={isOpen}
 		>
-		<a
-			href="/services"
-			aria-current={$page.url.pathname === '/services'}
-			class="text-white hover:text-gray-300">SERVICES</a
-		>
-		<a
-			href="/blogs"
-			aria-current={$page.url.pathname === '/blogs'}
-			class="text-white hover:text-gray-300">BLOGS</a
-		>
-		<a
-			href="/about"
-			aria-current={$page.url.pathname === '/about'}
-			class="text-white hover:text-gray-300">ABOUT</a
-		>
+			<span class="sr-only">Open main menu</span>
+			<svg
+				class="w-5 h-5"
+				aria-hidden="true"
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 17 14"
+			>
+				<path
+					stroke="currentColor"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="M1 1h15M1 7h15M1 13h15"
+				/>
+			</svg>
+		</button>
+		<div class="{isOpen ? '' : 'hidden'} w-full md:block md:w-auto" id="navbar-default">
+			<ul
+				class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700"
+			>
+				{#if user !== undefined}
+					<li>
+						<a href="/admin/dashboard" aria-current={$page.url.pathname === '/admin/dashboard'}>
+							Dashboard
+						</a>
+					</li>
+				{/if}
+				<li>
+					<a
+						href="/courses"
+						aria-current={$page.url.pathname === '/courses'}
+						class={$page.url.pathname === '/courses'
+							? 'block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500'
+							: 'block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'}
+					>
+						Courses
+					</a>
+				</li>
+				<li>
+					<a
+						href="/services"
+						aria-current={$page.url.pathname === '/services'}
+						class={$page.url.pathname === '/services'
+							? 'block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500'
+							: 'block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'}
+					>
+						Services
+					</a>
+				</li>
+				<li>
+					<a
+						href="/blogs"
+						aria-current={$page.url.pathname === '/blogs'}
+						class={$page.url.pathname === '/blogs'
+							? 'block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500'
+							: 'block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'}
+					>
+						Blogs
+					</a>
+				</li>
+				<li>
+					<a
+						href="/about"
+						aria-current={$page.url.pathname === '/about'}
+						class={$page.url.pathname === '/about'
+							? 'block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500'
+							: 'block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'}
+					>
+						About
+					</a>
+				</li>
+			</ul>
+		</div>
 	</div>
 </nav>
