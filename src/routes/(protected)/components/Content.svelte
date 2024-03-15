9ed09d04f901bf28f69effd5a1ae794d3a36ab96
GiorgiUbiria <ubiriagiorgi8@gmail.com>
2024-03-15 22:52:24 +0400

    Removed list and grid view modifier in the dashboard.

--- src/routes/(protected)/components/Content.svelte
+++ src/routes/(protected)/components/Content.svelte
@@ -25,60 +25,6 @@
 			<p class="text-2xl font-bold">{title}</p>
 			<p class="">{day + '-' + month + '-' + year}</p>
 		</div>
-		<div class="flex flex-wrap justify-between items-center pb-8">
-			<div class="flex items-center mt-4 md:mt-0">
-				<button
-					class="text-white rounded-md {$view == 'grid'
-						? 'bg-transparent'
-						: 'bg-gray-700'} p-2 ml-2"
-					on:click={() => changeView()}
-					title="List View"
-				>
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						width="24"
-						height="24"
-						viewBox="0 0 24 24"
-						fill="none"
-						stroke="currentColor"
-						stroke-width="2"
-						stroke-linecap="round"
-						stroke-linejoin="round"
-					>
-						<line x1="8" y1="6" x2="21" y2="6" />
-						<line x1="8" y1="12" x2="21" y2="12" />
-						<line x1="8" y1="18" x2="21" y2="18" />
-						<line x1="3" y1="6" x2="3.01" y2="6" />
-						<line x1="3" y1="12" x2="3.01" y2="12" />
-						<line x1="3" y1="18" x2="3.01" y2="18" />
-					</svg>
-				</button>
-				<button
-					class="text-white rounded-md {$view == 'grid'
-						? 'bg-gray-700'
-						: 'bg-transparent'} p-2 ml-2"
-					on:click={() => changeView()}
-					title="Grid View"
-				>
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						width="20"
-						height="20"
-						viewBox="0 0 24 24"
-						fill="none"
-						stroke="currentColor"
-						stroke-width="2"
-						stroke-linecap="round"
-						stroke-linejoin="round"
-					>
-						<rect x="3" y="3" width="7" height="7" />
-						<rect x="14" y="3" width="7" height="7" />
-						<rect x="14" y="14" width="7" height="7" />
-						<rect x="3" y="14" width="7" height="7" />
-					</svg>
-				</button>
-			</div>
-		</div>
 		<Table {rows} cols={columns} />
 	</div>
 
