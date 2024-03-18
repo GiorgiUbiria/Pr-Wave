2a1e898ff6129e00a725cabfb75dd17d990638e1
GiorgiUbiria <ubiriagiorgi8@gmail.com>
2024-03-18 22:15:46 +0400

    Modified the code to check the issues with redirection - 8.

--- src/routes/(protected)/admin/dashboard/+layout.server.ts
+++ src/routes/(protected)/admin/dashboard/+layout.server.ts
@@ -1,11 +1,11 @@
+import { redirect } from "@sveltejs/kit";
 import type { LayoutServerLoad } from "./$types";
-import { goto } from "$app/navigation";
 
 export const load: LayoutServerLoad = async ({ locals: { supabase } }) => {
     const userFetch = await supabase.auth.getUser();
 
     if (!userFetch.data.user) {
         console.log("There's no user")
-        goto("/")
+        redirect(303, "/about")
     }
 }
