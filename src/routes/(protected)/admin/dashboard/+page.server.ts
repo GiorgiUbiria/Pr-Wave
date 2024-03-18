11b7393b94f8a42a601193fa79fa2dbe3b7cabf1
GiorgiUbiria <ubiriagiorgi8@gmail.com>
2024-03-18 22:54:44 +0400

    Modified the code to check the issues with redirection - 12

--- src/routes/(protected)/admin/dashboard/+page.server.ts
+++ src/routes/(protected)/admin/dashboard/+page.server.ts
@@ -1,1 +1,12 @@
+import { redirect } from "@sveltejs/kit";
+import type { PageServerLoad } from "../$types";
+
 export const prerender = true
+
+export const load: PageServerLoad = async ({ locals: { supabase } }) => {
+    const userFetch = await supabase.auth.getUser();
+
+    if (!userFetch.data.user) {
+        redirect(303, "/")
+    }
+}
---
+++ src/routes/(protected)/admin/dashboard/settings/+page.server.ts
@@ -0,0 +1,10 @@
+import { redirect } from "@sveltejs/kit";
+import type { PageServerLoad } from "../$types";
+
+export const load: PageServerLoad = async ({ locals: { supabase } }) => {
+    const userFetch = await supabase.auth.getUser();
+
+    if (!userFetch.data.user) {
+        redirect(303, "/")
+    }
+}
