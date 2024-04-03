c055820cd66f43164c2b110394b10d570c2bb707
GiorgiUbiria <ubiriagiorgi8@gmail.com>
2024-04-03 16:52:53 +0400

    Add - Created supabase client configuration file

---
+++ src/config/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from '@supabase/supabase-js'
+
+const url = import.meta.env.PUPBLIC_SUPABASE_URL;
+const anonKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY;
+
+const supabase = createClient(url, anonKey)
+
+export default supabase
