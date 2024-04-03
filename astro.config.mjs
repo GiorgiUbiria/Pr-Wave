09c0ffe5167e5aa38a87a2f53d788fab60060974
GiorgiUbiria <ubiriagiorgi8@gmail.com>
2024-04-03 16:04:46 +0400

    Update - Removed SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,8 @@ import mdx from "@astrojs/mdx";
 import icon from "astro-icon";
 import lit from "@astrojs/lit";
 
-import node from "@astrojs/node";
-
-// https://astro.build/config
 export default defineConfig({
-  output: 'server',
   site: 'https://prwave.netlify.app/',
   sitemap: true,
   integrations: [sitemap(), mdx(), lit(), icon()],
-  adapter: node({
-    mode: "standalone"
-  })
 });
