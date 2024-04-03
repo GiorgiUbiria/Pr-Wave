29209dde4c304e6db0a671b020f818d858dfd4e6
GiorgiUbiria <ubiriagiorgi8@gmail.com>
2024-04-03 16:13:36 +0400

    Fix - modified the links leading to the landing page

--- src/config/nav.js
+++ src/config/nav.js
@@ -1,18 +1,18 @@
 export const nav = [
-	{
-		title: 'მთავარი',
-		slug: '/',
-	},
-	{
-		title: 'ბლოგი',
-		slug: '/blog',
-	},
-	{
-		title: 'ჩვენ შესახებ',
-		slug: '/company/about',
-	},
-	{
-		title: 'კონტაქტი',
-		slug: '/company/contact',
-	},
+    {
+        title: 'მთავარი',
+        slug: '/',
+    },
+    {
+        title: 'ბლოგი',
+        slug: '/blog',
+    },
+    {
+        title: 'ჩვენ შესახებ',
+        slug: '/company/about',
+    },
+    {
+        title: 'კონტაქტი',
+        slug: '/company/contact',
+    },
 ];
