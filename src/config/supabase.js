7f1c1b5ab73d4195bad9af496cf83d47cbdc8182
GiorgiUbiria <ubiriagiorgi8@gmail.com>
2024-04-04 15:18:51 +0400

    Fix - Corrected the environment variable names for Supabase client

--- src/config/supabase.js
+++ src/config/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const url = import.meta.env.PUBLIC_SUPABASE_URL;
-const anonKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY;
+const url = import.meta.env.SUPABASE_URL;
+const anonKey = import.meta.env.SUPABASE_ANON_KEY;
 
 const supabase = createClient(url, anonKey)
 
