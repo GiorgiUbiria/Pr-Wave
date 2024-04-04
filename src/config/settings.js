74cb9ed47dafa36494e96efbebc57dc0b656bcc1
GiorgiUbiria <ubiriagiorgi8@gmail.com>
2024-04-04 15:24:45 +0400

    Fix - Corrected the site URL in settings.json file

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,7 +1,7 @@
 export default {
     title: `Website for the PR Wave Social Marketing team, providing different services, courses and pleasant environment`,
     description: `A simple, clean, and modern theme for a startup or businesses' marketing website.`,
-    url: `https://prwave.netlify.app`,
+    url: `https://ornate-moonbeam-c5e191.netlify.app/`,
     name: `Pr Wave`,
     enableThemeSwitcher: true,
     showLittleSticksPlug: true,
