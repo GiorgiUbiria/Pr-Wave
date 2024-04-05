8cd06cc1442025e89d40c1b020d0161355244c96
GiorgiUbiria <ubiriagiorgi8@gmail.com>
2024-04-05 16:41:27 +0400

    Test - Changed theme switcher visually to slider toggle. Added images directory to src. Deleted some of the unwanted assets

--- src/components/theme-switcher/theme-switcher.ts
+++ src/components/theme-switcher/theme-switcher.ts
@@ -2,127 +2,130 @@ import { LitElement, html, css } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 
 import {
-    darkThemeIcon,
-    blueThemeIcon,
-} from './icons';
+	darkThemeIcon,
+	blueThemeIcon,
+} from './icons.js';
 
 const themes = [
-    {
-        name: 'ocean',
-        icon: blueThemeIcon,
-        label: 'ოკეანური',
-    },
-    {
-        name: 'dark',
-        icon: darkThemeIcon,
-        label: 'ბნელი',
-    },
+ {
+    name: 'dark',
+    icon: darkThemeIcon,
+    label: 'Dark',
+ },
+ {
+    name: 'ocean',
+    icon: blueThemeIcon,
+    label: 'Ocean',
+ },
 ]
 
 @customElement('theme-switcher')
 export class ThemeSwitcher extends LitElement {
-    static styles = [
-        css`
+	static styles = [
+		css`
 			:host {
 				display: block;
 			}
-			button {
-				display: inline-flex;
-				outline: none;
-				border: none;
-				background-color: transparent;
-				border: 2px solid transparent;
-				border-radius: 20rem;
-				padding: 1px;
+			.switch {
+				position: relative;
+				display: inline-block;
+				width: 60px;
+				height: 34px;
+                margin-top: 15px;
+			}
+			.switch input {
+				opacity: 0;
+				width: 0;
+				height: 0;
+			}
+			.slider {
+				position: absolute;
 				cursor: pointer;
-				transition: border var(--theme-transition);
+				top: 0;
+				left: 0;
+				right: 0;
+				bottom: 0;
+				background-color: #ccc;
+				-webkit-transition: .4s;
+				transition: .4s;
 			}
-			button[active] {
-				border: 2px solid var(--theme-primary);
-        box-shadow: 0 0 12px 1px var(--theme-primary);
+			.slider:before {
+				position: absolute;
+				content: "";
+				height: 26px;
+				width: 26px;
+				left: 4px;
+				bottom: 4px;
+				background-color: white;
+				-webkit-transition: .4s;
+				transition: .4s;
 			}
-			button:hover {
-				border: 2px solid var(--theme-primary);
+			input:checked + .slider {
+				background-color: #2196F3;
 			}
-			.theme-switcher__container {
-				margin: 2rem 0;
-				display: grid;
-				grid-template-columns: repeat(5, 1fr);
+			input:focus + .slider {
+				box-shadow: 0 0 1px #2196F3;
 			}
-			.theme-select__container {
-				display: flex;
-				flex-direction: column;
-				align-items: center;
-				justify-content: center;
+			input:checked + .slider:before {
+				-webkit-transform: translateX(26px);
+				-ms-transform: translateX(26px);
+				transform: translateX(26px);
 			}
-			.theme-select__container p {
-				font-size: var(--font-size-sm);
+			.slider.round {
+				border-radius: 34px;
+			}
+			.slider.round:before {
+				border-radius: 50%;
 			}
 		`,
-    ];
+	];
 
-    // set the _doc element
-    private _doc = document.firstElementChild;
+	private _doc = document.firstElementChild;
 
-    @property({ type: String })
-    theme: string | null = null;
+	@property({ type: String })
+	theme: string | null = null;
 
-    private _getCurrentTheme() {
-        // check for a local storage theme first
-        const localStorageTheme = localStorage.getItem('theme');
-        if (localStorageTheme !== null) {
-            this._setTheme(localStorageTheme);
-        } else {
-            // Set default theme to dark if the operating system specifies this preference
-            if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-                this._setTheme('dark');
-            } else { // Set to default/light theme if no specification, or light theme is specified
-                this._setTheme('ocean');
-            }
+	private _getCurrentTheme() {
+		const localStorageTheme = localStorage.getItem('theme');
+		if (localStorageTheme !== null) {
+			this._setTheme(localStorageTheme);
+		} else {
+			if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+				this._setTheme('dark');
+			} else {
+				this._setTheme('ocean');
+			}
+		}
+	}
 
-        }
-    }
+	firstUpdated() {
+		this._getCurrentTheme();
+	}
 
-    firstUpdated() {
-        this._getCurrentTheme();
-    }
+	private _setTheme(theme) {
+		this._doc.setAttribute('data-theme', theme);
+		const _heroImage = document.querySelector('#home-hero-image') as HTMLImageElement;
+		if (theme === 'dark') {
+			_heroImage.src = './src/images/dark-hero.jpg';
+		}
+		if (theme === 'ocean') {
+			_heroImage.src = './src/images/ocean-hero.jpg';
+		}
+		localStorage.setItem('theme', theme);
+		this.theme = theme;
+	}
 
-    private _setTheme(theme) {
-        this._doc.setAttribute('data-theme', theme);
+	private _toggleTheme() {
+		this.theme = this.theme === 'dark' ? 'ocean' : 'dark';
+		this._setTheme(this.theme);
+	}
 
-        const _heroImage = document.querySelector('#home-hero-image') as HTMLImageElement;
-
-        if (theme === 'dark') {
-            _heroImage.src = '/assets/images/home/dark-hero.jpg';
-        }
-        if (theme === 'ocean') {
-            _heroImage.src = '/assets/images/home/ocean-hero.jpg';
-        }
-
-        localStorage.setItem('theme', theme);
-        this.theme = theme;
-    }
-
-    render() {
-        const themeButtons = html`${themes.map((theme) => {
-            return html`
-      <div class="theme-select__container">
-        <button
-          @click=${() => this._setTheme(theme.name)}
-          ?active=${this.theme === theme.name}
-          title=${`Enable ${theme.label} Theme`}
-        >
-          ${theme.icon}
-        </button>
-        <p>${theme.label}</p>
-        </div>
-      `
-        })}`
-
-        return html`
-			<div class="theme-switcher__container">
-				${themeButtons}
-			</div>
+	render() {
+		return html`
+			<label class="switch">
+				<input type="checkbox" ?checked=${this.theme === 'ocean'} @change=${this._toggleTheme}>
+				<span class="slider round"></span>
+			</label>
 		`;
-    }
+	}
 }
