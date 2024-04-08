1a61b60e813d6e0271c12f1aedf8f63bdbd291cd
GiorgiUbiria <ubiriagiorgi8@gmail.com>
2024-04-08 22:35:25 +0400

    Add - Configured individual course page and course data

---
+++ src/pages/company/courses/courses.js
@@ -0,0 +1,12 @@
+export const courses = [
+    {
+        title: "ხანგრძლივი კურსი",
+        description: "ხანგრძლივი კურსის აღწერა",
+        url: "/company/courses/grdzeli"
+    },
+    {
+        title: "მოკლე კურსი",
+        description: "მოკლე კურსის აღწერა",
+        url: "/company/courses/mokle"
+    },
+]
