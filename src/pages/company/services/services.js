9bae1e20821b76ec4f4dcae98b4d30cb38f80b42
GiorgiUbiria <ubiriagiorgi8@gmail.com>
2024-04-08 22:28:29 +0400

    Add - Data file for services. Props for the Card component. Individual slug pages for course and service. Configured individual course page

---
+++ src/pages/company/services/services.js
@@ -0,0 +1,25 @@
+export const longTermServices = [
+    {
+        title: "პირველი ხანგრძლივი სერვისი",
+        description: "პირველი ხანგრძლივი სერვისის აღწერა",
+        url: "pirveli-grdzeli"
+    },
+    {
+        title: "მეორე ხანგრძლივი სერვისი",
+        description: "მეორე ხანგრძლივი სერვისის აღწერა",
+        url: "meore-grdzeli"
+    },
+]
+
+export const oneOffServices = [
+    {
+        title: "პირველი ერთჯერადი სერვისი",
+        description: "პირველი ერთჯერადი სერვისის აღწერა",
+        url: "pirveli-erteuli"
+    },
+    {
+        title: "მეორე ერთჯერადი სერვისი",
+        description: "მეორე ერთჯერადი სერვისის აღწერა",
+        url: "meore-erteuli"
+    },
+]
