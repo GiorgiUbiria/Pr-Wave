951db95499812ab051b428b66fb7e7529c0abda3
GiorgiUbiria <ubiriagiorgi8@gmail.com>
2024-04-11 18:16:47 +0400

    Fix - Moved syllabus arrays to course objects. Added Video instead of just a box for individual course page

--- src/pages/company/courses/courses.js
+++ src/pages/company/courses/courses.js
@@ -2,16 +2,142 @@ export const courses = [
     {
         title: "SMM PLUS | სოციალური მედიის მარკეტინგის კურსი",
         description: "SMM PLUS | სოციალური მედიის მარკეტინგის კურსის აღწერა",
-        url: "/company/courses/grdzeli"
+        url: "/company/courses/grdzeli",
+        syllabus: [
+            {
+                day: 1,
+                content: "Doing something on day 1"
+            },
+            {
+                day: 2,
+                content: "Doing something on day 2"
+            },
+            {
+                day: 3,
+                content: "Doing something on day 3"
+            },
+            {
+                day: 4,
+                content: "Doing something on day 4"
+            },
+            {
+                day: 5,
+                content: "Doing something on day 5"
+            },
+            {
+                day: 6,
+                content: "Doing something on day 6"
+            },
+            {
+                day: 7,
+                content: "Doing something on day 7"
+            },
+            {
+                day: 8,
+                content: "Doing something on day 8"
+            },
+            {
+                day: 9,
+                content: "Doing something on day 9"
+            },
+            {
+                day: 10,
+                content: "Doing something on day 10"
+            },
+        ]
     },
     {
         title: "კონტენტ-მარკეტინგის ექსპრესს კურსი",
         description: "კონტენტ-მარკეტინგის ექსპრესს კურსის აღწერა",
-        url: "/company/courses/mokle"
+        url: "/company/courses/mokle",
+        syllabus: [
+            {
+                day: 1,
+                content: "Doing something on day 1"
+            },
+            {
+                day: 2,
+                content: "Doing something on day 2"
+            },
+            {
+                day: 3,
+                content: "Doing something on day 3"
+            },
+            {
+                day: 4,
+                content: "Doing something on day 4"
+            },
+            {
+                day: 5,
+                content: "Doing something on day 5"
+            },
+            {
+                day: 6,
+                content: "Doing something on day 6"
+            },
+            {
+                day: 7,
+                content: "Doing something on day 7"
+            },
+            {
+                day: 8,
+                content: "Doing something on day 8"
+            },
+            {
+                day: 9,
+                content: "Doing something on day 9"
+            },
+            {
+                day: 10,
+                content: "Doing something on day 10"
+            },
+        ]
     },
     {
         title: "რეკლამირების ექსპრესს კურსი",
         description: "რეკლამირების ექსპრესს კურსის აღწერა",
-        url: "/company/courses/mokle-erteuli"
+        url: "/company/courses/mokle-erteuli",
+        syllabus: [
+            {
+                day: 1,
+                content: "Doing something on day 1"
+            },
+            {
+                day: 2,
+                content: "Doing something on day 2"
+            },
+            {
+                day: 3,
+                content: "Doing something on day 3"
+            },
+            {
+                day: 4,
+                content: "Doing something on day 4"
+            },
+            {
+                day: 5,
+                content: "Doing something on day 5"
+            },
+            {
+                day: 6,
+                content: "Doing something on day 6"
+            },
+            {
+                day: 7,
+                content: "Doing something on day 7"
+            },
+            {
+                day: 8,
+                content: "Doing something on day 8"
+            },
+            {
+                day: 9,
+                content: "Doing something on day 9"
+            },
+            {
+                day: 10,
+                content: "Doing something on day 10"
+            },
+        ]
     },
 ]
