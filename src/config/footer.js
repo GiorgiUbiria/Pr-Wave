314826d7ee3dbee64fe56b00e76b7071c429494c
GiorgiUbiria <ubiriagiorgi8@gmail.com>
2024-04-13 21:40:06 +0400

    Fix - Updated footer and the settings configuration

--- src/config/footer.js
+++ src/config/footer.js
@@ -1,22 +1,22 @@
 export const footerSocials = [
     {
-        name: 'Twitter',
-        url: 'https://linkedin.com/prwave',
+        name: 'Linkedin',
+        url: 'https://tr.linkedin.com/in/leila-berdzenishvilikahveci',
         icon: "mdi:linkedin",
     },
     {
-        name: 'Youtube',
-        url: 'https://youtube.com/c/prwave',
+        name: 'Tiktok',
+        url: 'https://www.tiktok.com/@pr_marketing_agency?fbclid=IwAR2Ed-z84TNpSCvp6ktBMOcBAwxFsVZg3bUP-JU0qtwehmg_a1A_DrP3lpY_aem_ATAtd5QSSuZJSHSZLftXMHEF32lrnagNQ-R2L1dKhfggW3BtRfbmbUZSWdsC9JVknYyfUdQ1LhVvHgOFWUhrKU0a',
         icon: "mdi:youtube",
     },
     {
         name: 'Instagram',
-        url: 'https://instagram.com/prwave',
+        url: 'https://www.instagram.com/pr_wave_/?fbclid=IwAR0Cv4U6Wnq0ebwdqDyjrHSZ3eQF9OsMADfqWJhnkGJ0i7f9SXaFD9azFu4_aem_ATBYhk_wdSQGvoYr7R-KnKKWC58F7rpEfzuggcpOig06hC_weH39yXsQOX-hSsrONKlliYavLfuhBDVU2ipSWfrC',
         icon: "mdi:instagram",
     },
     {
         name: 'Facebook',
-        url: 'https://facebook.com/prwave',
+        url: 'https://www.facebook.com/socialmediagencyprwave',
         icon: "ic:baseline-facebook",
     },
 ]
--- src/config/settings.js
+++ src/config/settings.js
@@ -1,8 +1,8 @@
 export default {
     title: `Website for the PR Wave Social Marketing team, providing different services, courses and pleasant environment`,
     description: `A simple, clean, and modern theme for a startup or businesses' marketing website.`,
-    url: `https://ornate-moonbeam-c5e191.netlify.app/`,
-    name: `Pr Wave`,
+    url: `https://prwave.net/`,
+    name: `PR WAVE`,
     enableThemeSwitcher: true,
     showLittleSticksPlug: true,
 };
